f751777f9ef65efd1112e985a0a88f1e9d10dbcb
SriRawin <srirawin1510@gmail.com>
2023-10-02 23:28:20 +0530

    - basic operations added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,128 @@
-// import NumericButton from "./compnents/NumericButton";
+import { useState } from "react";
+import NumericButton from "./compnents/NumericButton";
 
 function App() {
-  // const onClickNumbericButton = (digit: number) => {
-  //   console.log(digit);
-  // };
-  // const [count, setCount] = useState(0);
-  // #202226
-  // #2A2C30
+  const [value, setValue] = useState("");
+  const onClickNumbericButton = (item: string) => {
+    console.log(item);
+    if (item == "AC") {
+      setValue("");
+      return;
+    }
+    if (item == "DEL") {
+      setValue(value.slice(0, -1));
+      return;
+    }
+    if (item == "=") {
+      setValue(eval(value));
+      return;
+    }
+
+    setValue(value + item);
+  };
+
   return (
-    <div class=" flex flex-col items-center justify-start h-screen w-screen">
-      <div class="h-5"></div>
-      <p class="font-Montserrat text-6xl font-bold">Calculator</p>
-      <div class="h-5"></div>
-      <div class="bg-[#202226] h-96 w-96 rounded-lg"> </div>
+    <div className=" flex flex-col items-center justify-start h-screen w-screen ">
+      <div className="h-5"></div>
+      <p className="font-Montserrat text-6xl font-bold">Calculator</p>
+      <div className="h-5"></div>
+      <div className="bg-[#202226] rounded-lg px-4 py-6">
+        <form action="">
+          <div className="flex justify-end mx-1 my-2">
+            <input
+              className="h-12 text-end flex-1 px-2"
+              type="text"
+              value={value}
+            />
+          </div>
+          <div>
+            <NumericButton
+              digit={"AC"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"DEL"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"%"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"/"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+          </div>
+          <div>
+            <NumericButton
+              digit={"7"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"8"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"9"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"*"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+          </div>
+          <div>
+            <NumericButton
+              digit={"4"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"5"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"6"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"-"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+          </div>
+          <div>
+            <NumericButton
+              digit={"1"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"2"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"3"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"+"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+          </div>
+          <div>
+            <NumericButton
+              digit={"0"}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"."}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+            <NumericButton
+              digit={"="}
+              onClick={onClickNumbericButton}
+            ></NumericButton>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/compnents/NumericButton.tsx
+++ src/compnents/NumericButton.tsx
@@ -1,21 +1,18 @@
-
 import React from "react";
 
 interface NumericButtonProps {
-  digit: number;
-  onClick: (digit: number) => void;
+  digit: string;
+  onClick: (digit: string) => void;
 }
 
 const NumericButton: React.FC<NumericButtonProps> = (props) => {
   return (
-    <div>
-  
-
-        {/* onClick={() => {
-          props.onClick(props.digit);
-        }} */}
-
-    </div>
+    <input
+      type="button"
+      value={props.digit}
+      className="bg-[#363636] font-medium w-14 h-10 m-1 cursor-pointer rounded-xl hover:bg-slate-400 "
+      onClick={() => props.onClick(props.digit)}
+    />
   );
 };
 export default NumericButton;
